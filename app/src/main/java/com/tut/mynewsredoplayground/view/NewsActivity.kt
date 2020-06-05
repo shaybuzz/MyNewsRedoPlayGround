ce75ee2c180112cf11dc57e389f3fd829d2a4d2c
Shay Buzz <shaybuzz@gmail.com>
2020-06-05 17:16:16 +0300

    viewmodel2

--- app/src/main/java/com/tut/mynewsredoplayground/view/NewsActivity.kt
+++ app/src/main/java/com/tut/mynewsredoplayground/view/NewsActivity.kt
@@ -3,6 +3,7 @@ package com.tut.mynewsredoplayground.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.tut.mynewsredoplayground.R
 import com.tut.mynewsredoplayground.database.NewsDatabase
 import com.tut.mynewsredoplayground.network.NetworkService
@@ -12,10 +13,23 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
+    lateinit var newsViewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        val api = NetworkService.api
+        val dao = NewsDatabase.getInstance(this).getNewsDao()
+        val repo = ArticleRepositoryImpl(api, dao)
+        newsViewModel = ViewModelProvider(this, NewsViewModelFactory(repo)).get(NewsViewModel::class.java)
+
+        newsViewModel.articles.observe(this, Observer {
+            Timber.d("### got \n\n\n $it \n\n\n ### got size ${it.size}")
+        })
+
+        newsViewModel.fetchArticles()
 
         //testApi()
         //testRepo()
--- app/src/main/java/com/tut/mynewsredoplayground/view/NewsViewModel.kt
+++ app/src/main/java/com/tut/mynewsredoplayground/view/NewsViewModel.kt
@@ -1,4 +1,42 @@
 package com.tut.mynewsredoplayground.view
 
-class NewsViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.tut.mynewsredoplayground.repositories.ArticleRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.lang.IllegalArgumentException
+
+class NewsViewModel(private val articleRepository: ArticleRepository) : ViewModel() {
+
+    val articles = articleRepository.articles
+
+    private val job = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.IO + job)
+    private var page: Int = 1
+
+    fun fetchArticles() {
+        coroutineScope.launch {
+            articleRepository.fetch(page = page++)
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+
+}
+
+@Suppress("UNCHECKED_CAST")
+class NewsViewModelFactory(val articleRepository: ArticleRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(NewsViewModel::class.java)) {
+            return NewsViewModel(articleRepository) as T
+        }
+        throw IllegalArgumentException()
+    }
+
 }
