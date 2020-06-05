14c310d7fb85753fb60914df09eec23e16b0c1a8
Shay Buzz <shaybuzz@gmail.com>
2020-06-05 17:52:31 +0300

    add nav graph first fragment

---
+++ app/src/main/java/com/tut/mynewsredoplayground/view/TopNewsFragment.kt
@@ -0,0 +1,19 @@
+package com.tut.mynewsredoplayground.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tut.mynewsredoplayground.databinding.TopNewsBinding
+
+class TopNewsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = TopNewsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
