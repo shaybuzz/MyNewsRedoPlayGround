0574e62a4dbd5c0f6cccf294e783bdb203c81316
Shay Buzz <shaybuzz@gmail.com>
2020-06-09 00:18:09 +0300

    supprot https

--- app/src/main/java/com/tut/mynewsredoplayground/network/NewsApi.kt
+++ app/src/main/java/com/tut/mynewsredoplayground/network/NewsApi.kt
@@ -1,7 +1,9 @@
 package com.tut.mynewsredoplayground.network
 
 import com.tut.mynewsredoplayground.model.NewsResponse
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 //https://newsapi.org/v2/top-headlines?country=us&apiKey=74e4c5c9a4904ac68ca7e90e99a7336f
@@ -14,9 +16,24 @@ interface NewsApi {
         @Query("page") page: Int = 1
     ): NewsResponse
 
+    @GET("v2/top-headlines")
+    suspend fun getArticlesRes(
+        @Query("country") country: String = "us",
+        @Query("page") page: Int = 1
+    ): Response<NewsResponse>
+
     @GET("v2/everything")
     suspend fun search(
         @Query("q") searchNews: String,
         @Query("page") page: Int = 1
     ): NewsResponse
+
+
+    @GET("v2/top-headlines/{id}")
+    suspend fun getParam(
+        @Path("id") id: Int? = 66,
+        @Query("country") country: String = "us",
+        @Query("page") page: Int = 1
+    ): Response<NewsResponse>
+
 }
